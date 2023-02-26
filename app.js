a4183b5c5e38c63f02297394cda4934800189f83
shebl <shebblloll@gmail.com>
2023-02-26 11:36:09 +0200

    add to local storage and get form it

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let btnSubmit = document.querySelector(".container .form .add");
 let tasksDiv = document.querySelector(".container .tasks ");
 let tasksArray = [];
 /////////////////////////////////////////////////////////////////////////
+getTaskFormLocal();
 ///////////// [2] function when click on btn submit ////////////////////
 btnSubmit.addEventListener("click", () => {
   if (input.value == "") {
@@ -12,6 +13,7 @@ btnSubmit.addEventListener("click", () => {
     addTaskToArray(input.value);
     input.value = "";
     addTaskToPage(tasksArray);
+    addTaskToLocal(tasksArray);
   }
 });
 /////////////////////////////////////////////////////////////////////////
@@ -33,8 +35,34 @@ function addTaskToPage(tasksArray) {
     let taskTag = document.createElement("div");
     taskTag.appendChild(document.createTextNode(task.title));
     taskTag.classList.add("task");
+    if (task.completed) {
+      taskTag.classList.add("done");
+    }
     taskTag.setAttribute("data-id", task.id);
+    let delSpan = document.createElement("span");
+    delSpan.classList.add("delete");
+    delSpan.appendChild(document.createTextNode("X"));
+    taskTag.appendChild(delSpan);
     tasksDiv.appendChild(taskTag);
   });
+}
+/////////////////////////////////////////////////////////////////////////
+///////////// [5] function add task to local storage ////////////////////
+function addTaskToLocal(tasksArray) {
+  window.localStorage.setItem("task", JSON.stringify(tasksArray));
+}
+/////////////////////////////////////////////////////////////////////////
+///////////// [6] function get task from local storage ////////////////////
+function getTaskFormLocal() {
+  let data = window.localStorage.getItem("task");
+  if (data) {
+    let tasks = JSON.parse(data);
+    // console.log(tasks);
+    tasksArray = tasks;
+    addTaskToPage(tasksArray);
+    // tasks.forEach((task) => {
+    //   tasksArray.push(task);
+    // });
+  }
 }
 /////////////////////////////////////////////////////////////////////////
